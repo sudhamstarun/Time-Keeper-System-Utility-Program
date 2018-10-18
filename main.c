002740c3726e369fd5a69e43c1e8b7e5221f44fd
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2018-10-18 18:57:55 +0800

    added parsing function

--- main.c
+++ main.c
@@ -2,8 +2,53 @@
 FileName: main.c
 Student Name: Tarun Sudhams
 Student Number: 3035253876
-Development Platform: MACOSX 10.14 with gcc compiler and Sublime Text
-Compilation:
-Remarks
+Development Platform: MACOSX 10.14 with gcc compiler and Sublime Text(tested under Ubuntu 18.04 (academy servers via x2go))
+Compilation: gcc timekeeper_3015234567.c –o timekeeper
+Remarks: 
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+void parsing(char* input, char** argv)
+{
+	while(*input != '\0')
+	{
+		while(*input == ' ' || *input == '\t' || *input == '\n')
+		{
+			*input++;
+		}
+
+		*argv++ = input
+
+		while(*input != '\0' && *input != ' ' && *input = '\t' && *input != '\n')
+		{
+			input++;
+		}
+	}
+
+	*argv = '\0';
+}
+
+
+int main(int argc, char* argv[])
+{
+
+	/*Stage One Begins here*/
+
+	argv[1+argc] = NULL;
+
+	execvp(argv[0], argv);
+
+    /*Stage One Ends here*/
+
+    return 0;
+
+
+
+}
