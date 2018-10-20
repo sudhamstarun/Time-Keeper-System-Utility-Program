b192e069acd8a21b6adf67c60ee6a5de794506ec
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2018-10-20 12:58:35 +0800

    cleaned int main and expanded parse function

--- timekeeper.c
+++ timekeeper.c
@@ -4,7 +4,7 @@ Student Name: Tarun Sudhams
 Student Number: 3035253876
 Development Platform: MACOSX 10.14 with gcc compiler and Sublime Text(tested under Ubuntu 18.04 (academy servers via x2go))
 Compilation: gcc timekeeper_3015234567.c –o timekeeper
-Remarks: 
+Remarks:
 */
 
 #include <stdio.h>
@@ -12,14 +12,38 @@ Remarks:
 #include <string.h>
 #include <stdlib.h>
 
-char **parse(char *line)
+char **parse(int argc, char * argv[])
 {
+
+    int i; int strsize = 0;
+
+    for (i=1; i < argc; i++)
+    {
+        strsize += strlen(argv[i]);
+        if (argc > i+1)
+            strsize++;
+    }
+
+    char *cmdstring;
+    cmdstring = malloc(strsize);
+    cmdstring[0] = '\0';
+
+    for (i=1; i<argc; i++)
+    {
+        strcat(cmdstring, argv[i]);
+
+        if (argc > i+1)
+        {
+            strcat(cmdstring, " ");
+        }
+    }
+
     char **command = malloc(8 * sizeof(char *));
     char *separator = " ";
     char *parsed;
     int index = 0;
 
-    parsed = strtok(line, separator);
+    parsed = strtok(cmdstring, separator);
     while (parsed != NULL)
     {
         command[index] = parsed;
@@ -30,7 +54,7 @@ char **parse(char *line)
 
     command[index] = NULL;
 
-    return command;              
+    return command;
 }
 
 void execute(char **argv)
@@ -42,19 +66,19 @@ void execute(char **argv)
 
  	if (c_pid == 0)
  	{
-    	
 
-    	printf("Child: executing ls\n");
 
-    	//execute ls                                                                                                                                                               
+    	printf("Child: executing\n");
+
+    	//execute ls
     	execvp(argv[0], argv);
-    	//only get here if exec failed                                                                                                                                             
+    	//only get here if exec failed
     	perror("execve failed");
   	}
 
   	else if (c_pid > 0)
   	{
-    	
+
 
     	if( (pid = wait(&status)) < 0)
     	{
@@ -75,45 +99,16 @@ void execute(char **argv)
 
 int main(int argc, char *argv[])
 {
-	// Preparsing Begins Here
-
-	if (argc < 1)
+	if (argc < 2)
 	{
 		return 0;
 	}
 
-    int i; int strsize = 0;
-
-    for (i=1; i < argc; i++)
-    {
-        strsize += strlen(argv[i]);
-        if (argc > i+1)
-            strsize++;
-    }
-
-    char *cmdstring;
- 	char **command;
-    cmdstring = malloc(strsize);
-    cmdstring[0] = '\0';
-
-    for (i=1; i<argc; i++)
-    {
-        strcat(cmdstring, argv[i]);
-
-        if (argc > i+1)
-        {
-        	strcat(cmdstring, " ");
-        }
-    }
-
-    // Preparsing endds here
-
     //Stage One Begins Here
-
-    command = parse(cmdstring);
-    execute(command);  
-
+    char** command;
+    command = parse(argc,argv);
+    execute(command);
     //Stage One Ends Here
 
-	return 0; 
+	return 0;
 }
